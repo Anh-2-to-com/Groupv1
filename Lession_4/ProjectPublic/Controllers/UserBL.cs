7a3e97011b6aa4c7ca92dfdfb07d9ed0cea17460
DucHien2302 <hienprohienvip@gmail.com>
2024-09-10 15:45:22 +0700

    CommitThird

---
+++ Lession_4/ProjectPublic/Controllers/UserBL.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lib;
+using System.Data.SqlClient;
+using ProjectPublic.Models;
+
+namespace ProjectPublic.Controllers
+{
+    public class UserBL
+    {
+        public string pConnectionString { get; set; }
+        private SQLClass db;
+
+        public UserBL()
+        {
+            db = new SQLClass();
+            db.CreateConnection(pConnectionString);
+        }
+        public User getUser(string userName, string password)
+        {
+            User nd = new User();
+            string sql = "SELECT * FROM tb_user WHERE username = "+userName+" AND password = "+password+"";
+            nd = (User)db.ExecuteScalar(sql);
+            return nd;
+        }
+    }
+}
